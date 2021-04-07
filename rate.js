c468e21d3b474daa0389af31d307cb6b55c1164c
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2021-04-07 20:11:37 +0530

    Corrected description and version

--- rate.js
+++ rate.js
@@ -3,30 +3,28 @@ console.log("Working");
 window.onhashchange = function () {
   console.log("#changed", window.location.hash);
   window.onhashchange = function () {
-
     console.log("Running");
-    window.addEventListener('DOMContentLoaded', (event) => {
-        console.log('DOM fully loaded and parsed');
+    window.addEventListener("DOMContentLoaded", (event) => {
+      console.log("DOM fully loaded and parsed");
     });
 
-    setTimeout(function(){  var x = document.getElementsByClassName("bot-star float-left");
-    x[2].click();
-    var y = document.getElementsByClassName("like-dislike-item");
-    var z1 = y[0].getElementsByClassName(
-      "material-icons thumb_down float-right"
-    );
-    z1[0].click();
-    var z2 = y[1].getElementsByClassName(
-      "material-icons thumb_down float-right"
-    );
-    z2[0].click();
-    var z3 = y[2].getElementsByClassName(
-      "material-icons thumb_down float-right"
-    );
-    z3[0].click();
-    document.getElementById("vidRatingCancel").click();}, 300);
-
-
-  
+    setTimeout(function () {
+      var x = document.getElementsByClassName("bot-star float-left");
+      x[2].click();
+      var y = document.getElementsByClassName("like-dislike-item");
+      var z1 = y[0].getElementsByClassName(
+        "material-icons thumb_down float-right"
+      );
+      z1[0].click();
+      var z2 = y[1].getElementsByClassName(
+        "material-icons thumb_down float-right"
+      );
+      z2[0].click();
+      var z3 = y[2].getElementsByClassName(
+        "material-icons thumb_down float-right"
+      );
+      z3[0].click();
+      document.getElementById("vidRatingCancel").click();
+    }, 300);
   };
 };
