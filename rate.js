91b6ccae45f6914203da546cd1321ce1c2152ac7
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2021-04-07 20:09:46 +0530

    Fixed some major errors

--- rate.js
+++ rate.js
@@ -1,11 +1,32 @@
 console.log("Working");
-var x=document.getElementsByClassName("bot-star float-left");
-    x[0].click();
+
+window.onhashchange = function () {
+  console.log("#changed", window.location.hash);
+  window.onhashchange = function () {
+
+    console.log("Running");
+    window.addEventListener('DOMContentLoaded', (event) => {
+        console.log('DOM fully loaded and parsed');
+    });
+
+    setTimeout(function(){  var x = document.getElementsByClassName("bot-star float-left");
+    x[2].click();
     var y = document.getElementsByClassName("like-dislike-item");
-    var z1 = y[0].getElementsByClassName("material-icons thumb_down float-right");
+    var z1 = y[0].getElementsByClassName(
+      "material-icons thumb_down float-right"
+    );
     z1[0].click();
-    var z2 = y[1].getElementsByClassName("material-icons thumb_down float-right");
+    var z2 = y[1].getElementsByClassName(
+      "material-icons thumb_down float-right"
+    );
     z2[0].click();
-    var z3 = y[2].getElementsByClassName("material-icons thumb_down float-right");
-    z3[0].click(); 
-    document.getElementById("vidRatingCancel").click();
+    var z3 = y[2].getElementsByClassName(
+      "material-icons thumb_down float-right"
+    );
+    z3[0].click();
+    document.getElementById("vidRatingCancel").click();}, 300);
+
+
+  
+  };
+};
